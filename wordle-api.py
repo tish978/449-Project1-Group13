5579da039d111f64b9aa31092c0d8747ec9f216a
tish978 <35079405+tish978@users.noreply.github.com>
2022-10-22 09:38:55 -0700

    update of sqlalchemy downgrade for fetch() to work

--- wordle-api.py
+++ wordle-api.py
@@ -6,7 +6,6 @@ from databases import Database
 import socket
 from dataclasses import dataclass
 import json
-import wget
 import sqlite3
 import correctWords
 import validWords
@@ -48,12 +47,8 @@ def not_found(e):
 def not_found(e):
     return {"Error": "Game is Over"}, 505
 
-@app.route("/")
-async def index():
-    return "hello"
 
-
-@app.route("/register", methods=["POST"])
+@app.route("/register/", methods=["POST"])
 async def create_user():
     db = await _get_db()
     data = await request.get_json()
@@ -74,7 +69,7 @@ async def create_user():
 
 
   
-@app.route("/login", methods=["POST"])
+@app.route("/login/", methods=["POST"])
 async def login():
 
     db = await _get_db()
@@ -86,55 +81,31 @@ async def login():
     entered_pass = data['password']
     
     query = "SELECT * FROM users WHERE user_id = :id and password = :password"
-    row = await db.execute(query=query, values={"id": entered_id, "password": entered_pass})
+    row = await db.fetch_one(query=query, values={"id": entered_id, "password": entered_pass})
     if row:
     	return jsonify({"authenticated": "true"})  
+    else:
+    	abort(404)
     
 
 
-@app.route("/login-TEST", methods=["POST"])
-async def login_test():
+@app.route("/create_new_game/", methods=["POST"])
+async def create_new_game():
     db = await _get_db()
+    
+    
     data = await request.get_json()
-
-    user_data = f"{data['user_id']} {data['password']}"
+    user_data = f"{data['user_id']}"
     app.logger.debug(user_data)
     entered_id = data['user_id']
-    entered_pass = data['password']
-
-    query = "SELECT * FROM users WHERE user_id = :id AND password = :pass;"
-    row = await db.execute(query=query, values={"id": entered_id, "pass": entered_pass})
-    if row:
-        return jsonify({"authenticated": "true"})
-    else:
-        abort(404)
     
-      
-        
-        
-@app.route("/login/<int:id><string:password>", methods=["GET"])
-async def login_user(id, password):
-    db = await _get_db()
-    data = await request.get_json()
-    book = await db.execute("SELECT * FROM users WHERE user_id = :user_id AND password = :password", values={"user_id": id, "password": password})
-    if book:
-        return jsonify({"authenticated": "true"})
-    else:
-        abort(404)        
-        
-
-
-
-@app.route("/create_new_game/<int:id>", methods=["POST"])
-async def create_new_game(id):
-    db = await _get_db()
-
+   
     secretWord = str(random.choice(correctWords.correctWord))
     print("SECRET WORD: " + secretWord)
 
 
     query = "INSERT INTO games (game_id, game_secret_word, won, number_of_guesses_made, number_of_guesses_left, user_id) VALUES (NULL, :word, :won, :made, :left, :user_id)"
-    await db.execute(query=query, values={"word": secretWord, "won": False, "made": 0, "left": 6, "user_id": id})
+    await db.execute(query=query, values={"word": secretWord, "won": False, "made": 0, "left": 6, "user_id": entered_id})
     return jsonify({"Success": "New Game Entered"})
 
 
@@ -149,7 +120,7 @@ async def correct_word_load():
         await db.execute(query=query,
                          values={"correctWord": word})
 
-    return "All correct words loaded into DB"    
+    return jsonify({"Success":"All correct words loaded into DB"})    
     
 
 
@@ -164,9 +135,7 @@ async def valid_word_load():
         await db.execute(query=query,
                          values={"validWord": word})
 
-    return "All valid words loaded into DB"
-
-
+    return jsonify({"Success":"All valid words loaded into DB"}) 
 
 
 
@@ -186,38 +155,43 @@ async def answer():
     if len(answer) > 5:
     		abort(404)
     
-
-    connection = sqlite3.connect("////home/student/Documents/449-wordle/wordle-DB")
-    print("DB connected")
-    guessCheck = "SELECT number_of_guesses_made FROM games WHERE game_id = ?"
-    initCursor = connection.execute(guessCheck, [game_id])
-    for guessMade in initCursor:
-    	guess_count = guessMade[0]
+ 
+    query = "SELECT number_of_guesses_made FROM games WHERE game_id = :game_id"
+    initCursor = await db.fetch_one(query=query, values={"game_id": game_id})
+    #for guessMade in initCursor:
+    if initCursor:
+    	print("guess_count: " + str(guess_count))
+    	print("initCursor " + str(initCursor))
+    	guess_count = initCursor[0]
     	if guess_count == 6:
     		abort(505) 
 
     
-    sql = "SELECT game_secret_word FROM games WHERE game_id = ?"
-    cursor = connection.execute(sql, [game_id])
-    for row in cursor:
-    	if answer == row[0]:
-    		guess_count = guess_count + 1
-    		guesses_left = 6 - guess_count
-    		count_update = "UPDATE games SET number_of_guesses_made=:guess_count WHERE game_id=:game_id"
-    		await db.execute(query=count_update, values={"guess_count": guess_count, "game_id": game_id})
-    		guesses_left_update = "UPDATE games SET number_of_guesses_left=:guesses_left WHERE game_id=:game_id"
-    		await db.execute(query=guesses_left_update, values={"guesses_left": guesses_left, "game_id": game_id})
-    		victory_update = "UPDATE games SET won=:won WHERE game_id=:game_id"
-    		await db.execute(query=victory_update, values={"won": 1, "game_id": game_id})
-    		return jsonify({"VICTORY": "Correct word!"})
+    sql = "SELECT game_secret_word FROM games WHERE game_id = :game_id"
+    cursor = await db.fetch_val(query=sql, values={"game_id": game_id})
+    #for row in cursor:
+    	#if answer == row[0]:
+    if cursor:
+    	print("cursor: " + str(cursor))
+    	if answer == cursor:
+    	   print("yes")
+    	   guess_count = guess_count + 1
+    	   guesses_left = 6 - guess_count
+    	   count_update = "UPDATE games SET number_of_guesses_made=:guess_count WHERE game_id=:game_id"
+    	   await db.execute(query=count_update, values={"guess_count": guess_count, "game_id": game_id})
+    	   guesses_left_update = "UPDATE games SET number_of_guesses_left=:guesses_left WHERE game_id=:game_id"
+    	   await db.execute(query=guesses_left_update, values={"guesses_left": guesses_left, "game_id": game_id})
+    	   victory_update = "UPDATE games SET won=:won WHERE game_id=:game_id"
+    	   await db.execute(query=victory_update, values={"won": 1, "game_id": game_id})
+    	   return jsonify({"VICTORY": "Correct word!"})
     	else:
-    		guess_count = guess_count + 1
-    		guesses_left = 6 - guess_count
-    		count_update = "UPDATE games SET number_of_guesses_made=:guess_count WHERE game_id=:game_id"
-    		await db.execute(query=count_update, values={"guess_count": guess_count, "game_id": game_id})
-    		guesses_left_update = "UPDATE games SET number_of_guesses_left=:guesses_left WHERE game_id=:game_id"
-    		await db.execute(query=guesses_left_update, values={"guesses_left": guesses_left, "game_id": game_id})
-    		return jsonify({"Incorrect": "Number of guesses is increased", "Number of guesses made": guessCount, "Number of guesses left": guesses_left})
+    	   guess_count = guess_count + 1
+    	   guesses_left = 6 - guess_count
+    	   count_update = "UPDATE games SET number_of_guesses_made=:guess_count WHERE game_id=:game_id"
+    	   await db.execute(query=count_update, values={"guess_count": guess_count, "game_id": game_id})
+    	   guesses_left_update = "UPDATE games SET number_of_guesses_left=:guesses_left WHERE game_id=:game_id"
+    	   await db.execute(query=guesses_left_update, values={"guesses_left": guesses_left, "game_id": game_id})
+    	   return jsonify({"Incorrect": "Number of guesses is increased", "Number of guesses made": guessCount, "Number of guesses left": guesses_left})
 
 
 
@@ -234,10 +208,9 @@ async def get_games_in_progress():
     app.logger.debug(user_data)
     entered_id = data['user_id']
 
-    connection = sqlite3.connect("////home/student/Documents/449-wordle/wordle-DB")
-    print("DB connected")
-    query = "SELECT * FROM games WHERE user_id = ? AND won = ?"
-    initCursor = connection.execute(query, [entered_id, 0])
+    
+    query = "SELECT * FROM games WHERE user_id = :user_id AND won = :won"
+    initCursor = await db.fetch_all(query=query, values={"user_id": entered_id, "won": 0})
     for game in initCursor:
     	id_of_game = game[0]
     	gamesList.append(id_of_game)
@@ -262,13 +235,11 @@ async def get_game_state():
     app.logger.debug(user_data)
     entered_id = data['game_id']
 
-    connection = sqlite3.connect("////home/student/Documents/449-wordle/wordle-DB")
-    print("DB connected")
-    query = "SELECT * FROM games WHERE game_id = ?"
-    initCursor = connection.execute(query, [entered_id])
-    for game in initCursor:
-    	guessesMade = game[3]
-    	won = game[2]
+    query = "SELECT * FROM games WHERE game_id = :game_id"
+    initCursor = await db.fetch_one(query=query, values={"game_id": entered_id})
+    if initCursor:
+    	guessesMade = initCursor[3]
+    	won = initCursor[2]
     	if won == 1:
     		guessesLeft = 6 - guessesMade
     		return jsonify({"Game State": "GAME OVER", "Guesses Made": guessesMade, "Guesses Left": guessesLeft})
