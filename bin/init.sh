6c8fd98970c13228a54a121dc524540f688a0ac3
Shivangi Shakya <shivangi11061993@gmail.com>
2022-11-17 15:45:42 -0800

    Removed wordle-api.py

--- bin/init.sh
+++ bin/init.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-sqlite3 ./var/wordle-DB.db < ./share/wordle-DB_schema.sql
-sqlite3 ./var/users-DB.db < ./share/users-DB_schema.sql
-sqlite3 ./var/games-DB.db < ./share/games-DB_schema.sql
+sqlite3 ./var/users-DB < ./share/users-DB_schema.sql
+sqlite3 ./var/games-DB < ./share/games-DB_schema.sql
